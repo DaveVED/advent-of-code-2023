e39e8ca7a1bdb0fa0167b2a735f73e245d5c85e8
DaveVED <dave.denns@gs.com>
2023-12-02 00:44:25 -0600

    :sparkles: day two

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { solveDayOne } from './day-one/day-one';
+import { solveDayTwo } from './day-two/day-two';
 
 // Day 1
-console.log('DAY 1...');
 const dayOneFilePath = './src/day-one/inputs/puzzle-input.txt';
 const dayOneAnswer: number = solveDayOne(dayOneFilePath);
-console.log(`Day one answer is ${dayOneAnswer}`);
+console.log(`Day ONE answer is: ${dayOneAnswer}`);
+
+// Day 2
+const dayTwoFilePath = './src/day-two/inputs/puzzle-input.txt';
+const dayTwoAnswer: number = solveDayTwo(dayTwoFilePath);
+console.log(`Day TWO answer is: ${dayTwoAnswer}`);
